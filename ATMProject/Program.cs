576777d1e043a32a299220c914a6575d5201b5b4
Furkan SARMUSAK <furkansarmusak@gmail.com>
2023-12-04 20:07:44 +0300

    Add files via upload

---
+++ ATMProject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.Write("Çekmek istediğiniz tutarı girin: ");
+        int cekilecekTutar = Convert.ToInt32(Console.ReadLine());
+
+        Dictionary<int, int> banknotlar = CalculateBanknote(cekilecekTutar);
+
+        Console.WriteLine("Çekilen banknotlar:");
+        if (banknotlar.Count > 0)
+        {
+            string outputMessage = string.Empty;
+            foreach (var banknot in banknotlar)
+            {
+                outputMessage += $"{banknot.Value} adet {banknot.Key} TL.\n";
+                
+            }
+            Console.WriteLine(outputMessage);
+            Console.ReadLine();
+        }
+        
+    }
+
+    static Dictionary<int, int> CalculateBanknote(int tutar)
+    {
+        int[] banknotlar = { 200, 100, 50, 20, 10, 5};
+        Dictionary<int, int> cekilenBanknotlar = new Dictionary<int, int>();
+
+        foreach (var banknot in banknotlar)
+        {
+            if (tutar >= banknot)
+            {
+                int adet = tutar / banknot;
+                tutar %= banknot;
+                cekilenBanknotlar.Add(banknot, adet);
+            }
+        }
+
+        return cekilenBanknotlar;
+    }
+}
